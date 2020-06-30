ee298a3331f4d320305ae54717d3d40e1dcc544f
MasayaGit <pontamasaya@i.softbank.jp>
2020-07-01 02:15:44 +0900

    add ABC_172/C.go

---
+++ src/ABC_172/C.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n, m int
+	var k int64
+	fmt.Fscan(r, &n, &m, &k)
+	a := make([]int64, n+1)
+	b := make([]int64, m+1)
+	for i := 0; i < n; i++ {
+		var tmp int64
+		fmt.Fscan(r, &tmp)
+		a[i+1] = a[i] + tmp
+	}
+	for i := 0; i < m; i++ {
+		var tmp int64
+		fmt.Fscan(r, &tmp)
+		b[i+1] = b[i] + tmp
+	}
+	var ans int
+	j := m
+	for i := 0; i <= n; i++ {
+		if a[i] > k {
+			break
+		}
+		for b[j] > k-a[i]{
+			j--
+		}
+		if i+j > ans {
+			ans = i + j
+		}
+	}
+	fmt.Println(ans)
+}
