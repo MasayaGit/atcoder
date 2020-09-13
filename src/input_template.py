f24729c939d19fdb7c13ae0b0a54d7fb9098a3ae
MasayaGit <pontamasaya@i.softbank.jp>
2020-09-13 16:51:39 +0900

    add input_template

---
+++ src/input_template.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+x, y= map(int, input().split())
+
+a_list = list(map(int, input().split()))
+
+h_list = []
+for _ in range(n):
+    h_list.append(int(input()))
+
+for _ in range(M):
+    a, b = map(int, input().split())
